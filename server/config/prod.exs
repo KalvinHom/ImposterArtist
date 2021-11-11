e3a0551c40faccad498d9ef8b89b5cc0fa6ad719
Kalvin Hom <kalvin.hom@gmail.com>
2021-11-11 08:38:27 -0800

    Add port 443 for ssl websockets

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -10,8 +10,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :imposter_artist, ImposterArtistWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost"],
+  http: [port: {:system, 80}],
+  https: [port: 443]
 
 # Do not print debug messages in production
 config :logger, level: :info
